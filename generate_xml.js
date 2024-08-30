134b0dd57758b0eb8e641301add6e54832cbf1a8
medienrocker <50263518+medienrocker@users.noreply.github.com>
2024-08-30 14:51:01 +0200

    Add New Entry works

--- generate_xml.js
+++ generate_xml.js
@@ -11,6 +11,30 @@ function generateXML(repetitions) {
     }
 }
 
+function generateTEXT(repetitions) {
+    const template = `[[slot{{num}}]]`;
+    let output = '';
+
+    for (let i = 1; i <= repetitions; i++) {
+        let formattedNumber = i.toString().padStart(3, '0');
+        output += template.replace('{{num}}', formattedNumber);
+
+        // Add a space between slots, except at the end of a line
+        if (i % 10 !== 0 && i !== repetitions) {
+            output += ' ';
+        }
+
+        // Add a line break after every 10 slots
+        if (i % 10 === 0 && i !== repetitions) {
+            output += '\n';
+        }
+    }
+
+    console.log(output);
+    return output; // In case you want to use the output elsewhere
+}
+
 // Example usage: generate XML 20 times
-generateXML(200);
+//generateXML(200);
+generateTEXT(200);
 exit
